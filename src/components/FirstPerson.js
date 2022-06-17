7f1c798458835c403487704b7a6dd091bf9c5dbd
KuDim89 <36006135+KuDim89@users.noreply.github.com>
2022-06-17 18:51:05 +0300

    has been added FirstPerson component

---
+++ src/components/FirstPerson.js
@@ -0,0 +1,42 @@
+import {useLayoutEffect, useState} from "react";
+import chatService from "../services/chat";
+
+const FirstPerson = () => {
+  const [chatState, setChatState] = useState(chatService.initialState)
+
+  useLayoutEffect(() => {
+    chatService.subscribe(setChatState);
+    chatService.init();
+  }, []);
+
+  const onFormSubmit = e => {
+    e.preventDefault();
+    const messageObject = {
+      person: 'first-person',
+      text: e.target.elements.messageInput.value.trim(),
+    }
+    chatService.sendMessage(messageObject);
+    document.getElementById('messageForm').reset();
+  }
+
+  return (
+      <div className="container">
+        <h2>Dmytro</h2>
+        <div className="chat-box">
+          {chatState.data.map( message => (
+              <div>
+                <p className={message.person}>{message.text}</p>
+                <div className="clear"></div>
+              </div>
+          ))}
+        </div>
+        <form id="messageForm" onSubmit={onFormSubmit}>
+          <input type="text" id="messageInput" name="messageInput" placeholder="type here..." required />
+          <button type="submit">Send</button>
+        </form>
+        <button className="clear-button" onClick={() => chatService.clearChat()}> Clear Chat</button>
+      </div>
+  );
+};
+
+export default FirstPerson;
