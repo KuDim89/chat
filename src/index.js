a8cab9760cc50c983e096a518c4577ebbe64ecbe
KuDim89 <36006135+KuDim89@users.noreply.github.com>
2022-06-17 18:50:23 +0300

    was configured routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import {createRoot} from 'react-dom/client';
 import './index.css';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import FirstPerson from "./components/FirstPerson";
+import App from "./App";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
     <BrowserRouter>
-
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index path='first-person' element={<FirstPerson />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
 );
